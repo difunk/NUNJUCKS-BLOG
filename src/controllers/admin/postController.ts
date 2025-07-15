ddef4ea039e88e3e03fb933d882472f67e85a2ba
Dirk <so0mewhat2@gmail.com>
2025-07-15 12:55:21 +0200

    add: admin panel user interface

--- src/controllers/admin/postController.ts
+++ src/controllers/admin/postController.ts
@@ -1,5 +1,13 @@
 import type { Request, Response } from "express";
+import { getAllBlogEntries } from "../../models/blogEntriesModel";
+import { transformBlogEntriesData } from "../../utils/transformBlogData";
 
-export const postsListing = (req: Request, res: Response) => {
-  res.render("admin/indexPage.njk", {});
+export const postsListing = async (req: Request, res: Response) => {
+  const posts = await getAllBlogEntries();
+  const postsWithSlug = transformBlogEntriesData(posts);
+
+  res.render("admin/indexPage.njk", {
+    title: "Admin-Dashboard",
+    posts: postsWithSlug,
+  });
 };
